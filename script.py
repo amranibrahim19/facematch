2a3901a7d80f1a95c706f11182ebf849e98f0563
Amran Ibrahim <39895750+amranibrahim19@users.noreply.github.com>
2024-09-11 18:33:50 +0800

    improve facemeatch

--- script.py
+++ script.py
@@ -84,5 +84,5 @@ def compare_faces():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=8080, debug=True)
-    app.run()
+    app.run(host='0.0.0.0', port=8080, debug=True)
+    # app.run()
